a047c3c666881acac6c543b796c9d1e86c33703d
AJ1732 <ejemeniboi@gmail.com>
2024-10-03 19:17:53 +0100

    chore: upadte line height

--- src/components/sections/homepage/HeroSection/index.tsx
+++ src/components/sections/homepage/HeroSection/index.tsx
@@ -8,7 +8,7 @@ const HeroSection = () => {
         {/* <div className="fixed left-40 -z-[1] aspect-square w-80 rounded-full bg-purple-400/10 blur-lg" /> */}
 
         <div className="flex max-w-2xl flex-col items-center justify-center gap-4 rounded-3xl border border-neutral-800 bg-neutral-900 px-6 py-20 text-center sm:mx-8 sm:min-h-[calc(100dvh-14rem)] lg:max-h-[800px]">
-          <h1 className="font-disket-mono text-2xl md:text-5xl">
+          <h1 className="font-disket-mono text-2xl leading-8 md:text-5xl md:leading-[3.5rem]">
             Ensuring <span className="text-purple-500">Authenticity</span> in
             the <span className="text-purple-500"> Digital Art World</span>
           </h1>
